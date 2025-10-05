5f262aafc7d964154fcf92d81ae1198ee2bd5468
Nilay Namit <109901909+LuckyNilay15@users.noreply.github.com>
2025-10-05 16:18:51 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,7 +6,6 @@ export const auth = async (req, res, next) => {
     const token = req.headers.authorization?.startsWith("Bearer ")
       ? req.headers.authorization.split(" ")[1]
       : null;
-    console.log(token)
     if (!token) return res.status(401).json({ message: "No token provided" });
 
     const payload = jwt.verify(token, process.env.JWT_SECRET);
